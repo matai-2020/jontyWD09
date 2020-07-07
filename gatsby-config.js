9dcee16d3e78d0103640cf97d0b35790d67b8ffd
Jonty Buchan <63333241+jonty-terrence@users.noreply.github.com>
2020-07-07 16:48:10 +1200

    Get ready for deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,9 @@
 
 module.exports = {
   siteMetadata: {
-    title: `Pandas eating the things`,
+    title: `Pandas Eating Lots`,
+    description: `A simple description about pandas eating lots...`,
+    author: `jonty`,
   },
   plugins: [
     {
@@ -16,6 +18,7 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
@@ -23,5 +26,20 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
